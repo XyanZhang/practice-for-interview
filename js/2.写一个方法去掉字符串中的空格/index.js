5e7dc3a218921fd692e7522845bd127a058d16d0
zhangxinyan <zhangxinyan@huajin.com>
2022-01-26 17:36:20 +0800

    js/2.

---
+++ js/2.写一个方法去掉字符串中的空格/index.js
@@ -0,0 +1,18 @@
+// 正则
+var str = '  abc d e f  g ';
+function trim(str) {
+    var reg = /\s+/g;
+    if (typeof str === 'string') {
+        var trimStr = str.replace(reg,'');
+    }
+    console.log(trimStr)
+}
+trim(str)
+
+var trim = function(str){
+    return str.replace(/\s*/g,"");
+}
+str.replace(/\s*/g,""); //去除字符串内所有的空格
+str.replace(/^\s*|\s*$/g,""); //去除字符串内两头的空格
+str.replace(/^\s*/,""); //去除字符串内左侧的空格
+str.replace(/(\s*$)/g,""); //去除字符串内右侧的空格
